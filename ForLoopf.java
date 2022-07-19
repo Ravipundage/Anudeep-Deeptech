80e7008a19bd0e6951b0cd7927d9e2eec49c4e02
Ravipundage <99859589+Ravipundage@users.noreply.github.com>
2022-07-19 18:55:02 +0530

    Add files via upload

---
+++ ForLoopf.java
@@ -0,0 +1,12 @@
+class ForLoopf
+{
+     public static void main(String args[])
+{
+System.out.println("Printing The Value From 10 To 6");
+   for(int i=10; i>6; i--)
+
+ System.out.println("The value of i: "+ i);
+
+      }
+}
+   
