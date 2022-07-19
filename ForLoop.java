b5033ed81628652c4f863c024aae9198dac8439f
Ravipundage <99859589+Ravipundage@users.noreply.github.com>
2022-07-19 18:46:23 +0530

    Add files via upload

--- ForLoop.java
+++ ForLoop.java
@@ -2,8 +2,8 @@ class ForLoop
 {
      public static void main(String args[])
 {
-System.out.println("Printing The Value From 10 To 6");
-   for(int i=10; i>6; i--)
+System.out.println("Printing The Value From 11 To 3");
+   for(int i=11; i>3; i--)
 
  System.out.println("The value of i: "+ i);
 
