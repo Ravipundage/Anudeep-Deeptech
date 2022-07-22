c5c8397ae9c9eb82176ff350948d2fa5ece034e4
Ravipundage <99859589+Ravipundage@users.noreply.github.com>
2022-07-22 12:27:28 +0530

    Add files via upload

---
+++ Thursday Practical/StudentDetail.java
@@ -0,0 +1,27 @@
+class StudentDetail
+{
+    public static void main (String[]args)
+    {   
+        String id = "87654321";
+        String Name = "RAVIRAJ";
+        String Address = "Nagpur";
+	String PhoneNo = "9556864357";
+	String Gender = "Male";
+	String DOB = "022/7/1999";
+        String gpa = "9.5";
+	float Height = 175.5f ;
+	float weight = 72.5f;
+ 
+	System.out.println("Detail About Students : ");
+        System.out.println("Student id : " + id);
+	System.out.println("Name of the Student : " + Name);
+	System.out.println("Address of the Student : " + Address);
+	System.out.println("Phone Number of the Student : " + PhoneNo);
+	System.out.println("Gender of the Student : " + Gender);
+	System.out.println("Date of Birth of the Student : " + DOB);
+        System.out.println("Student gpa : " + gpa);
+	System.out.println("Height of the student : " + Height);
+	System.out.println("weight of the Student : " + weight);
+
+    }
+}
