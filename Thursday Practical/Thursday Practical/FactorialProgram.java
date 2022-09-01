dc544b59bc0e43937f2f816354955cf850fc36da
Ravipundage <99859589+Ravipundage@users.noreply.github.com>
2022-09-01 17:47:56 +0530

    Add files via upload

---
+++ Thursday Practical/Thursday Practical/FactorialProgram.java
@@ -0,0 +1,22 @@
+@FunctionalInterface
+interface factorial{
+	public void check (int a);
+	}
+
+          public class FactorialProgram{
+
+	  public static void main(String[] args) 
+          {
+	     	factorial fa=(int num)->{
+	        int fact=1;
+		for(int i=1; i<=num; i++)
+	  {
+		fact=fact*i;
+          }
+		  System.out.println("Factorial:"+fact);
+		
+          };
+		fa.check(10);
+	  }
+
+   }
