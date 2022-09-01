bb6290bee0a927827fff740f8ba2b3b089f9ca71
Ravipundage <99859589+Ravipundage@users.noreply.github.com>
2022-09-01 17:11:36 +0530

    Add files via upload

---
+++ Thursday Practical/Thursday Practical/HexatoDecimal.java
@@ -0,0 +1,11 @@
+    public class HexatoDecimal{
+
+	public static void main(String[] args) 
+        {
+	    String hexnum ="7A";
+	    int num =Integer.parseInt(hexnum,16);
+	    System.out.println("Decimal equivalent of given hex number: "+num);
+        }
+	
+
+ }
