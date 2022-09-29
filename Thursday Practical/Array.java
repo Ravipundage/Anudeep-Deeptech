f588d4c6e0e70e1fc87914b33cbf41e7d6b65ade
Ravipundage <99859589+Ravipundage@users.noreply.github.com>
2022-09-29 17:29:45 +0530

    Add files via upload

---
+++ Thursday Practical/Array.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+public class Array {
+
+		
+		
+		static void segregateElements(int arr[], int n)
+		{
+			
+			
+			int temp[] = new int[n];
+
+			
+			int j = 0; // index of temp
+			
+			for (int i = 0; i < n; i++)
+				if (arr[i] >= 0)
+					temp[j++] = arr[i];
+			if (j == n || j == 0)
+				return;
+
+			
+			for (int i = 0; i < n; i++)
+				if (arr[i] < 0)
+					temp[j++] = arr[i];
+			for (int i = 0; i < n; i++)
+				arr[i] = temp[i];
+		}
+		
+		public static void main(String arg[])
+		{
+			int arr[] = { 1, -1, 3, 2, -7, -5, 11, 6 };
+			int n = arr.length;
+
+			segregateElements(arr, n);
+
+			for (int i = 0; i < n; i++)
+				System.out.print(arr[i] + " ");
+		}
+	}
+
+	
+
+
+
